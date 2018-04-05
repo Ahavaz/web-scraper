44a04439ac9922182c53300bd1e5cfd41a44904a
Ahavaz <lucas.cesar.8@hotmail.com>
2018-04-05 16:22:57 -0300

    Added pdf2json

---
+++ scrapLinksPdf.js
@@ -0,0 +1,74 @@
+const request = require('request')
+// const cheerio = require('cheerio')
+const PDFParser = require('pdf2json')
+const fs = require('fs')
+
+// const path = `C:\\Users\\Forensics\\Documents\\repos\\web-scraper\\`
+// const filesArr = [
+// 	`links.json`,
+// 	// `links1k.json`,
+// 	// `links2k.json`,
+// 	// `links3k.json`,
+// 	// `links4k.json`,
+// 	// `links5k.json`,
+// 	// `links6k.json`,
+// 	// `links7k.json`,
+// 	// `links8k.json`,
+// 	// `links9k.json`,
+// 	// `links9.23k.json`,
+// 	// `linksTest.json`
+// ]
+// const jsonArr = [].concat(...filesArr.map(_ => require(`./files/${_}`)))
+const jsonArr = require(`./test.pdf`)
+let result = []
+let log = []
+let c = 1
+let pdfParser = new PDFParser();
+
+pdfParser.on('pdfParser_dataError', errData => 
+	console.error(errData.parserError))
+pdfParser.on('pdfParser_dataReady', pdfData => 
+	fs.writeFile('./testPdf.json', JSON.stringify(pdfParser.getAllFieldsTypes())))
+
+pdfParser.loadPDF('./testPdf.pdf')
+
+console.log(`Status\tID\tTotal\tURL`)
+
+for([i, url] of jsonArr.entries()) {
+	let link = url
+	let index = i
+	// getData(link, index)
+}
+
+function getData(link, index) {
+	setTimeout(function() {
+		request({ url: link, encoding: 'binary'}, function(error, response, body) {
+			if (error) throw new Error(error)
+			const $ = cheerio.load(body)
+			$('span.nome_parte').each(function(i, e) {
+				let tipo = $(e).parent().children('span.tipo_parte').text()
+				let nome = $(e).parent().children('span.nome_parte').text()
+				result.push({ Tipo: tipo, Nome: nome, URL: response.request.uri.href })
+			})
+			$('span.nome_parte_representante').each(function(i, e) {
+				let tipo = $(e).parent().children('span.tipo_parte_representante').text()
+				let nome = $(e).parent().children('span.nome_parte_representante').text()
+				result.push({ Tipo: tipo, Nome: nome, URL: response.request.uri.href })
+			})
+			$('table div.parte').parent().parent().each(function(i, e) {
+				let tipo = $(e).children().first().text()
+				let nome = $(e).children().last().text()
+				result.push({ Tipo: tipo, Nome: nome, URL: response.request.uri.href })
+			})
+			let status = `${(c*100/jsonArr.length).toFixed(0)}%`
+			log.push({ Status: status, ID: c, Total: result.length, URL: link })
+			console.log(`${status}\t${c}\t${result.length}\t${link}`)
+			if (c++ === jsonArr.length) {
+				fs.writeFileSync(`./files/result.json`, JSON.stringify(result))
+				fs.writeFileSync('log.json', JSON.stringify(log))
+				// console.log(result)
+				console.log('\nDone!')
+			}
+		})
+	}, 2000 * index)
+}
